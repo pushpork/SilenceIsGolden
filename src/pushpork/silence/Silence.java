fa2a11073f3825f6da875d7242c3d74d8f5d33b5
Joshua Pulham <jcpulham@googlemail.com>
2012-04-26 17:13:02 +0100

    basic Java2D setup

--- src/pushpork/silence/Silence.java
+++ src/pushpork/silence/Silence.java
@@ -1,7 +1,28 @@
 package pushpork.silence;
 
-public class Silence {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Silence extends JPanel {
+	// constants
+	public static final int WIDTH = 320;
+	public static final int HEIGHT = 200;
+	public static final String TITLE = "Silence Is Golden";
+	
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+	}
+   
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		JFrame frame = new JFrame(TITLE);
+		frame.add(new Silence());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 	}
 }
